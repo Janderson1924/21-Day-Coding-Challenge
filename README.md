50520262501517319062ef6f2231c667afc1cc77
Janderson1924 <justinanderson101@gmail.com>
2022-11-22 16:59:52 -0700

    initial commit, add completed challenges

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# 21-Day-Coding-Challenge
+# 21-Day Coding Challenge
+
+This Repo will host the files containing each daily challenge and it's subsequent solution, as part of the [Lighthouse Labs 21-Day Coding Challenge.](https://coding-challenge.lighthouselabs.ca/about)
+
+Done for the purposes of building daily coding habits, as well as polishing vanilla JS skills.
---
+++ daily-challenges/challenge1.js
@@ -0,0 +1,16 @@
+/* INSTRUCTIONS
+Our latest testing indicates a malfunctioning component in our communications apparatus. For some reason, messages from mission control are not being correctly displayed on board the shuttle. While the message is being displayed on the shuttle’s terminal, the sender’s name is not displayed beside it. Your first task is to write a function that will take two string parameters and return a string containing the sender name and the message in the form specified in the instructions.
+
+EXAMPLES
+Input:
+  parseMessage("Mission Control", "Hello there!")
+
+Output:
+  'Mission Control: Hello there!'
+
+
+SOLUTION */
+const parseMessage = (origin, message) => {
+  const parsedMessage = `${origin}: ${message}`
+  return parsedMessage
+}
